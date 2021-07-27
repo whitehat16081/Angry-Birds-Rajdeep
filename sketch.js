d12423527d2a30a7f002a1a9cd0182f2fdd9344b
whitehat16081 <whitehat.16081@gmail.com>
2021-07-27 12:37:49 +0530

    Platform added

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform=new Ground(100,280,200,200);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -59,4 +59,14 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
+
+
+
+//Working area-->add
+//staging area-->
+//commit
+
+
+
